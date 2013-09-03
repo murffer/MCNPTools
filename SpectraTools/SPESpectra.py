2f4bce6a843f74781dfea0ab3187f81992504d1b
Matthew Urffer <matthew.urffer@gmail.com>
2013-09-03 09:11:50 -0400

    Working on parsing file
    Parsing all of the data based on knowing keywords start with '$'

--- SpectraTools/SPESpectra.py
+++ SpectraTools/SPESpectra.py
@@ -5,9 +5,28 @@ class SPESpectra:
     """ File format from ORTEC Manual 
     
     Attributes:
-      
+      filename    - filename of the spectra
+      spec_id     - one line of text describing the data
+      spec_rem    - any number of lines containing remarks about the data
+      date_meas   - measurement date in the form mm/dd/yyyy hh:mm:ss
+      meas_tim    - live time and realtime of the spectrum 
+      data        - channel number and corresponding data
+      roi         - regions of intrest
+      energ_fit   - energy calibration (a+b*chn)
+      mca_cal     - energy calibration along with mca calibration
+      shape_cal   - FWHM calibration factors
     """
-    
     self.filename = filename
-    self.spec_id = ''         # One line of text describing the data
+    with open(filename,'r') as f:
+      data = f.read()
+    tokens = data.split('$')
+    for t in tokens:
+      # Getting the SPEC_ID
+      if t.lower.startswith('spec_id'):
+        content = t.splitlines()
+        for c in content:
+          if not c in t.lower.starts('spec_id'):
+            self.spec_id += c
+      elif t.lower.startswith('spec_rem'):
+
 
